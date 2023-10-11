5ba94940063b9560868971bfc0044aacc69a1049
icarop16 <pedroicaro161107@gmail.com>
2023-10-11 11:45:27 -0300

    Atividade Tabuada

---
+++ pedroicaro.py
@@ -0,0 +1,15 @@
+# Digite um numero interio e veja sua tabuada
+numero = int(input("Digite um numero inteiro:"))
+Tabuada0 = numero*0
+Tabuada1 = numero*1
+Tabuada2 = numero*2
+Tabuada3 = numero*3
+Tabuada4 = numero*4
+Tabuada5 = numero*5
+Tabuada6 = numero*6
+Tabuada7 = numero*7
+Tabuada8 = numero*8
+Tabuada9 = numero*9
+Tabuada10 = numero*10
+Tabuada_completa= [Tabuada0, Tabuada1, Tabuada2,Tabuada3, Tabuada4, Tabuada5, Tabuada6, Tabuada7, Tabuada8, Tabuada9, Tabuada10]
+print(Tabuada_completa)
